d2956cad2ab78e146e7ed68daf3d9bfe273cb540
Sohail Mahmud <sohailmahmud@yahoo.com>
2021-10-21 00:48:58 +0600

    Create colors.dart

---
+++ lib/src/styles/colors.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+// Colors
+
+const kBaseColor = Color(0xFFC21E56);
+const kButtonColor = Color(0xFFFF3399);
+const kBackgroundColor = Color(0xFFFFFFFF);
+const kConsultationColor = Color(0xFFD4D4D7);
+const kDCardColor = Color(0xFFD9DADC);
+const kOnlineColor = Color(0xFF6ECEC0);
+const kCardTitleColor = Color(0xFFCECFD1);
+const kDashBoxColor = Color(0xFFFAFAFA);
+const kResendBtnColor = Color(0xFF1888B8);
+const kTitleColor = Color(0xffd3d3d3);
+const kTitleTextColor = Color(0xFF303030);
+const kBodyTextColor = Color(0xFF4B4B4B);
+const kTextLightColor = Color(0xFF959595);
+const kInfectedColor = Color(0xFFFF8748);
+const kDeathColor = Color(0xFFFF4848);
+const kRecovercolor = Color(0xFF36C12C);
+const kPrimaryColor = Color(0xFF3382CC);
+final kShadowColor = const Color(0xFFB7B7B7).withOpacity(.16);
+final kActiveShadowColor = const Color(0xFF4056C6).withOpacity(.15);
+const kFaceBookColor = Color(0xFF102397);
+const kGoogleColor = Color(0xFFff4f38);
+const kWhiteShadow = Color(0xFFF5F5F5);
+const kWhiteShade = Color(0xFFF4F4F4);
+// Text Style
+const kHeadingTextStyle = TextStyle(
+  fontSize: 22,
+  fontWeight: FontWeight.w600,
+);
+
+const kFontHeading = TextStyle(fontFamily: '');
