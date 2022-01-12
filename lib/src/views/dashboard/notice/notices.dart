7ad1864935dea48311a0f889bdcc1328fda5c9d9
sohail <sohailmahmud@yahoo.com>
2022-01-12 18:00:08 +0600

    fix settings_ui and slidable

--- lib/src/views/dashboard/notice/notices.dart
+++ lib/src/views/dashboard/notice/notices.dart
@@ -1,8 +1,7 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:redid/src/styles/constants.dart';
 
 class Notices extends StatefulWidget {
@@ -10,44 +9,130 @@ class Notices extends StatefulWidget {
   static String tag = 'Notices';
 
   @override
-  NoticesState createState() => NoticesState();
+  NoticesScreenState createState() => NoticesScreenState();
 }
 
-class NoticesState extends State<Notices> with SingleTickerProviderStateMixin {
-  late final SlidableController slidableController;
-  final List<_HomeItem> items = List.generate(
-    20,
-    (i) => _HomeItem(
-      i,
-      'Title Notice $i',
-      _getSubtitle(i),
-      _getAvatarColor(i),
-    ),
-  );
+class NoticesScreenState extends State<Notices> {
+  String apiUrl = 'https://jsonplaceholder.typicode.com/albums/1/photos';
+  int pageNo = 1;
+  final ScrollController _scrollController = ScrollController();
+  bool isLoading = false;
+  List albums = [];
+  void _fetchData() async {
+    if (!isLoading) {
+      setState(() {
+        isLoading = true;
+      });
+      final response = await http.get(Uri.parse(apiUrl));
+      if (response.statusCode == 200) {
+        List albumList = [];
+        // ignore: prefer_typing_uninitialized_variables
+        var resultBody;
+        pageNo = (pageNo > 100) ? 1 : pageNo++;
+        apiUrl = "https://jsonplaceholder.typicode.com/albums/$pageNo/photos";
+        resultBody = jsonDecode(response.body);
+        for (int i = 0; i < resultBody.length; i++) {
+          albumList.add(resultBody[i]);
+        }
+        setState(() {
+          isLoading = false;
+          albums.addAll(albumList);
+        });
+      } else {
+        setState(() {
+          isLoading = false;
+        });
+      }
+    }
+  }
 
   @override
   void initState() {
-    slidableController = SlidableController(
-      onSlideAnimationChanged: handleSlideAnimationChanged,
-      onSlideIsOpenChanged: handleSlideIsOpenChanged,
-    );
+    _fetchData();
     super.initState();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _fetchData();
+      }
+    });
   }
 
-  void handleSlideAnimationChanged(Animation<double>? slideAnimation) {
-    setState(() {});
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 
-  void handleSlideIsOpenChanged(bool? isOpen) {
-    setState(() {});
+  Widget _buildProgressIndicator() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: Opacity(
+          opacity: isLoading ? 1.0 : 00,
+          child: const CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildList() {
+    return albums.isEmpty
+        ? Center(
+            child: Container(
+              padding: const EdgeInsets.all(5),
+              child: const Text(
+                'No new notifications.',
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+          )
+        : ListView.builder(
+            itemCount: albums.length + 1,
+            itemBuilder: (BuildContext context, int index) {
+              if (index == albums.length) {
+                return _buildProgressIndicator();
+              } else {
+                return Padding(
+                  padding: const EdgeInsets.only(
+                      left: 8.0, top: 2.0, bottom: 2.0, right: 8.0),
+                  child: Card(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: SizedBox(
+                      height: 80,
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage:
+                              NetworkImage(albums[index]['thumbnailUrl']),
+                        ),
+                        title: Text(
+                          (albums[index]['title']),
+                          style: const TextStyle(fontFamily: 'Book-Antiqua'),
+                        ),
+                        onTap: () {
+                          //print(albums[index]);
+                        },
+                      ),
+                    ),
+                  ),
+                );
+              }
+            },
+            controller: _scrollController,
+          );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false,
         backgroundColor: kBackgroundColor,
+        automaticallyImplyLeading: false,
         centerTitle: true,
         toolbarHeight: 50,
         elevation: 0.0,
@@ -55,366 +140,19 @@ class NoticesState extends State<Notices> with SingleTickerProviderStateMixin {
         leading: IconButton(
           icon: const FaIcon(FontAwesomeIcons.chevronCircleLeft),
           splashRadius: 25,
-          onPressed: () {
-            Navigator.pop(context);
-          },
+          onPressed: () => Navigator.of(context).pop(),
         ),
         title: const Text(
-          'Noticeboard',
-          style: TextStyle(
-            fontFamily: 'Chiller',
-            fontSize: 25,
-            color: kBaseColor,
-            fontWeight: FontWeight.w900,
-          ),
+          'Notifications',
+          style: kAppBarTextStyle,
         ),
       ),
-      body: Center(
-        child: OrientationBuilder(
-          builder: (context, orientation) => _buildList(
-              context,
-              orientation == Orientation.portrait
-                  ? Axis.vertical
-                  : Axis.horizontal),
-        ),
+      backgroundColor: kBackgroundColor,
+      body: Container(
+        padding: const EdgeInsets.only(top: 10.0),
+        child: _buildList(),
       ),
+      resizeToAvoidBottomInset: false,
     );
   }
-
-  Widget _buildList(BuildContext context, Axis direction) {
-    return ListView.builder(
-      scrollDirection: direction,
-      itemBuilder: (context, index) {
-        final Axis slidableDirection =
-            direction == Axis.horizontal ? Axis.vertical : Axis.horizontal;
-        var item = items[index];
-        if (item.index < 8) {
-          return _getSlidableWithLists(context, index, slidableDirection);
-        } else {
-          return _getSlidableWithDelegates(context, index, slidableDirection);
-        }
-      },
-      itemCount: items.length,
-    );
-  }
-
-  Widget _getSlidableWithLists(
-      BuildContext context, int index, Axis direction) {
-    final _HomeItem item = items[index];
-    //final int t = index;
-    return Slidable(
-      key: Key(item.title),
-      controller: slidableController,
-      direction: direction,
-      dismissal: SlidableDismissal(
-        child: const SlidableDrawerDismissal(),
-        onDismissed: (actionType) {
-          _showSnackBar(
-              context,
-              actionType == SlideActionType.primary
-                  ? 'Dismiss Archive'
-                  : 'Dimiss Delete');
-          setState(() {
-            items.removeAt(index);
-          });
-        },
-      ),
-      actionPane: _getActionPane(item.index)!,
-      actionExtentRatio: 0.25,
-      child: direction == Axis.horizontal
-          ? VerticalListItem(items[index])
-          : HorizontalListItem(items[index]),
-      actions: <Widget>[
-        IconSlideAction(
-          caption: 'Archive',
-          color: Colors.blue,
-          icon: Icons.archive,
-          onTap: () => _showSnackBar(context, 'Archive'),
-        ),
-        IconSlideAction(
-          caption: 'Share',
-          color: Colors.indigo,
-          icon: Icons.share,
-          onTap: () => _showSnackBar(context, 'Share'),
-        ),
-      ],
-      secondaryActions: <Widget>[
-        Container(
-          height: 800,
-          color: Colors.green,
-          child: const Text('a'),
-        ),
-        IconSlideAction(
-          caption: 'More',
-          color: Colors.grey.shade200,
-          icon: Icons.more_horiz,
-          onTap: () => _showSnackBar(context, 'More'),
-          closeOnTap: false,
-        ),
-        IconSlideAction(
-          caption: 'Delete',
-          color: Colors.red,
-          icon: Icons.delete,
-          onTap: () => _showSnackBar(context, 'Delete'),
-        ),
-      ],
-    );
-  }
-
-  Widget _getSlidableWithDelegates(
-      BuildContext context, int index, Axis direction) {
-    final _HomeItem item = items[index];
-
-    return Slidable.builder(
-      key: Key(item.title),
-      controller: slidableController,
-      direction: direction,
-      dismissal: SlidableDismissal(
-        child: const SlidableDrawerDismissal(),
-        closeOnCanceled: true,
-        onWillDismiss: (item.index != 10)
-            ? null
-            : (actionType) {
-                return showDialog<bool>(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: const Text('Delete'),
-                      content: const Text('Item will be deleted'),
-                      actions: <Widget>[
-                        TextButton(
-                          child: const Text('Cancel'),
-                          onPressed: () => Navigator.of(context).pop(false),
-                        ),
-                        TextButton(
-                          child: const Text('Ok'),
-                          onPressed: () => Navigator.of(context).pop(true),
-                        ),
-                      ],
-                    );
-                  },
-                ) as FutureOr<bool>;
-              } as FutureOr<bool> Function(SlideActionType?)?,
-        onDismissed: (actionType) {
-          _showSnackBar(
-              context,
-              actionType == SlideActionType.primary
-                  ? 'Dismiss Archive'
-                  : 'Dimiss Delete');
-          setState(() {
-            items.removeAt(index);
-          });
-        },
-      ),
-      actionPane: _getActionPane(item.index)!,
-      actionExtentRatio: 0.25,
-      child: direction == Axis.horizontal
-          ? VerticalListItem(items[index])
-          : HorizontalListItem(items[index]),
-      actionDelegate: SlideActionBuilderDelegate(
-          actionCount: 2,
-          builder: (context, index, animation, renderingMode) {
-            if (index == 0) {
-              return IconSlideAction(
-                caption: 'Archive',
-                color: renderingMode == SlidableRenderingMode.slide
-                    ? Colors.blue.withOpacity(animation!.value)
-                    : (renderingMode == SlidableRenderingMode.dismiss
-                        ? Colors.blue
-                        : Colors.green),
-                icon: Icons.archive,
-                onTap: () async {
-                  var state = Slidable.of(context);
-                  var dismiss = await (showDialog<bool>(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        title: const Text('Delete'),
-                        content: const Text('Item will be deleted'),
-                        actions: <Widget>[
-                          TextButton(
-                            child: const Text('Cancel'),
-                            onPressed: () => Navigator.of(context).pop(false),
-                          ),
-                          TextButton(
-                            child: const Text('Ok'),
-                            onPressed: () => Navigator.of(context).pop(true),
-                          ),
-                        ],
-                      );
-                    },
-                  ) as FutureOr<bool>);
-
-                  if (dismiss) {
-                    state!.dismiss();
-                  }
-                },
-              );
-            } else {
-              return IconSlideAction(
-                caption: 'Share',
-                color: renderingMode == SlidableRenderingMode.slide
-                    ? Colors.indigo.withOpacity(animation!.value)
-                    : Colors.indigo,
-                icon: Icons.share,
-                onTap: () => _showSnackBar(context, 'Share'),
-              );
-            }
-          }),
-      secondaryActionDelegate: SlideActionBuilderDelegate(
-          actionCount: 2,
-          builder: (context, index, animation, renderingMode) {
-            if (index == 0) {
-              return IconSlideAction(
-                caption: 'More',
-                color: renderingMode == SlidableRenderingMode.slide
-                    ? Colors.grey.shade200.withOpacity(animation!.value)
-                    : Colors.grey.shade200,
-                icon: Icons.more_horiz,
-                onTap: () => _showSnackBar(context, 'More'),
-                closeOnTap: false,
-              );
-            } else {
-              return IconSlideAction(
-                caption: 'Delete',
-                color: renderingMode == SlidableRenderingMode.slide
-                    ? Colors.red.withOpacity(animation!.value)
-                    : Colors.red,
-                icon: Icons.delete,
-                onTap: () => _showSnackBar(context, 'Delete'),
-              );
-            }
-          }),
-    );
-  }
-
-  static Widget? _getActionPane(int index) {
-    switch (index % 4) {
-      case 0:
-        return const SlidableBehindActionPane();
-      case 1:
-        return const SlidableStrechActionPane();
-      case 2:
-        return const SlidableScrollActionPane();
-      case 3:
-        return const SlidableDrawerActionPane();
-      default:
-        return null;
-    }
-  }
-
-  static Color? _getAvatarColor(int index) {
-    switch (index % 4) {
-      case 0:
-        return Colors.red;
-      case 1:
-        return Colors.green;
-      case 2:
-        return Colors.blue;
-      case 3:
-        return Colors.indigoAccent;
-      default:
-        return null;
-    }
-  }
-
-  static String? _getSubtitle(int index) {
-    switch (index % 4) {
-      case 0:
-        return 'SlidableBehindActionPane';
-      case 1:
-        return 'SlidableStrechActionPane';
-      case 2:
-        return 'SlidableScrollActionPane';
-      case 3:
-        return 'SlidableDrawerActionPane';
-      default:
-        return null;
-    }
-  }
-
-  void _showSnackBar(BuildContext context, String text) {
-    ScaffoldMessenger.of(context)
-      ..hideCurrentSnackBar()
-      ..showSnackBar(SnackBar(content: Text(text)));
-  }
-}
-
-class HorizontalListItem extends StatelessWidget {
-  const HorizontalListItem(this.item, {Key? key}) : super(key: key);
-  final _HomeItem item;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      width: 160.0,
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          Expanded(
-            child: CircleAvatar(
-              backgroundColor: item.color,
-              child: Text('${item.index}'),
-              foregroundColor: Colors.white,
-            ),
-          ),
-          Expanded(
-            child: Center(
-              child: Text(
-                item.subtitle!,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class VerticalListItem extends StatelessWidget {
-  const VerticalListItem(this.item, {Key? key}) : super(key: key);
-  final _HomeItem item;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 2,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      color: Colors.pink.shade50,
-      child: GestureDetector(
-        onTap: () =>
-            Slidable.of(context)?.renderingMode == SlidableRenderingMode.none
-                ? Slidable.of(context)?.open()
-                : Slidable.of(context)?.close(),
-        child: Container(
-          color: Colors.white,
-          child: ListTile(
-            leading: CircleAvatar(
-              backgroundColor: item.color,
-              child: Text('${item.index}'),
-              foregroundColor: Colors.white,
-            ),
-            title: Text(item.title),
-            subtitle: Text(item.subtitle!),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class _HomeItem {
-  const _HomeItem(
-    this.index,
-    this.title,
-    this.subtitle,
-    this.color,
-  );
-
-  final int index;
-  final String title;
-  final String? subtitle;
-  final Color? color;
 }
